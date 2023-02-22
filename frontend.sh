6a468ca62292b0d5a18fdefa148cc5fdbe7653a4
kthullib <kondalarao.thullibilli@oracle.com>
2023-02-22 16:34:47 +0530

    Modified

--- frontend.sh
+++ frontend.sh
@@ -2,19 +2,24 @@ code_dir=$(pwd)
 log_file=/tmp/roboshop.log
 rm -f ${log_file}
 
-echo -e "\e[35mInstalling nginx \e[0m"
+print_head(){
+echo -e "\e[35m$1\e[0m"
+}
+
+print_head "Installing nginx"
 yum install nginx -y  &>>${log_file}
 systemctl enable nginx 
 systemctl start nginx 
-echo -e "\e[35mRemving Old content \e[0m"
+
+print_head "Remving Old content"
 rm -rf /usr/share/nginx/html/* &>>${log_file}
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip  &>>${log_file}
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip &>>${log_file}
-echo -e "\e[35mCopying the config files \e[0m"
-cp ${code_dir}/configs/roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
 
-echo -e "\e[35mEnabling the nginx \e[0m"
+print_head "Copying the config files"
+cp ${code_dir}/configs/roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
+print_head "Enabling the nginx"
 systemctl enable nginx  &>>${log_file}
-echo -e "\e[35mRestarting the nginx \e[0m"
+print_head "Restarting the nginx"
 systemctl restart nginx &>>${log_file}
